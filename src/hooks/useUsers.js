29735f6fdef12559277debb1b6d77a2fa9ba50eb
tubi55 <tubi55@nate.com>
2023-12-26 11:57:12 +0900

    데이터 상태 주석 설명

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -10,9 +10,17 @@ export const useUserQuery = () => {
     queryFn: fetchUser,
     refetchOnWindowFocus: false, //다른 브라우저탭 이동했다시 다시 넘어왔을떄 refetching안함
     refetchOnMount: false, //재 마운트시 refetching안함
-    cacheTime: 1000 * 10,
+    gcTime: 1000 * 5,
   });
 };
+
+/*
+  fresh: 서버데이터를 최신상태로 인식하는 상태
+  stale: 서버데이터를 오래된 데이터로 인식하는 상태
+  inactive: 서버데이터가 더이상 해당 컴포넌트에서 활용되지 않는 상태
+  gcTime: inactive가 되자마다 gcTime에 등록된 시간값이 소진되면서 해당 시간이 지나면 가비지컬렉터가 해당 메모리 제거
+
+*/
 
 /*
   react-query 개발툴에서 확인할수 있는 상태값 5가지정리
