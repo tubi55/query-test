ebc2ec32ad043dca4974eaca58ffeb50bff34ae1
tubi55 <tubi55@nate.com>
2023-12-26 11:18:15 +0900

    query데이터를 호출하는 컴포넌트를 각 라우터로 분리

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,10 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 
 const fetchUser = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
   return await response.json();
 };
-
 export const useUserQuery = () => {
   return useQuery({ queryKey: ['user'], queryFn: fetchUser });
 };
+
+/*
+  react-query 개발툴에서 확인할수 있는 상태값 5가지정리
+  fresh: 비동기 데이터가 현재 최신상태 (refetching할 필요가 없는 신선한 상태)
+  fetching: 비동기 데이터의 요청중인 상태 (pending)
+  paused: 특정이유로 비동기 데이터 요청이 보류가 된상태
+  stale: 현재 해당 컴포넌트에서 활용되고 있는 데이터가 최신상태가 아닌경우 (refetching이 필요한 상태)
+  inactive: 최신상태가 아닌 데이터를 해당 컴포넌트에서 현재 활용되고 있지 않는 상태 (일정시간이후 해당 데이터가 삭제됨)
+
+
+
+*/
